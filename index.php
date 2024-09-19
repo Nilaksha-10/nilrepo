4425a05db3b56bb0c5783aa1f4ebb1b9d9c8fa16
Nilaksha-10 <nilakshaghosh79@gmail.com>
2024-09-19 11:28:55 +0530

    pushed php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// $name="I am a student";
+// echo str_word_count($name);
+// $small=" my name is Nil";
+// echo "<br>";
+// echo strtoupper($small);
+$var2 = "good morning";
+//echo "<br>";
+//echo $var2;
+echo "<br>";
+echo str_replace("morning","boobies",$var2);
+?>
